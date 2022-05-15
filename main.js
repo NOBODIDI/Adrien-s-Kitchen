b68a5e0c301e3dc218ff6927a0a8f204966f271a
Adrien Mery <86313865+NOBODIDI@users.noreply.github.com>
2022-05-16 01:26:33 +0200

    Added JS menu animation

---
+++ main.js
@@ -0,0 +1,18 @@
+const navIconElt = document.querySelector('nav__icon'); 
+const navCloseElt = document.querySelector('nav__close'); 
+const navList = document.querySelector('nav__list'); 
+const navBgOverlayElt = document.querySelector('nav__bgOverlay'); 
+
+const navOpen = () => {
+    navList.classList.add('show'); 
+    navBgOverlayElt.classList.add('active'); 
+    document.body.style = 'visibility: visible; height: 100vh; width: 100vh; overflow: hidden'; 
+}
+
+const navClose = () => {
+    navList.classList.remove('show'); 
+    navBgOverlayElt.classList.remove('active'); 
+    document.body.style = 'visibility: visible; height: initial; width: 100%; overflow-x: hidden'; 
+}
+
+navIconElt.addEventListener('click', navOpen); 
