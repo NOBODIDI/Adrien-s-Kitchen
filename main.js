4bce65ddcfd1e3d4d853a069293b43229556141a
Adrien Mery <86313865+NOBODIDI@users.noreply.github.com>
2022-05-16 16:28:47 +0200

    Finished footer and added hero section. The mobile menu still does not work fully (unclickable) so there is some code added to make sure it does not interfere w/ the rest).

--- main.js
+++ main.js
@@ -8,13 +8,34 @@ const navOpen = () => {
     navBgOverlayElt.classList.add('active'); 
     document.body.style = 'visibility: visible; height: 100vh; width: 100vw; overflow: hidden;'; 
 }
-
+if(navIconElt){ // to try to prevent errors, does not work 
+    navIconElt.addEventListener('click', () => {
+        console.log('btn clicked');
+    });
+}
 const navClose = () => {
     navList.classList.remove('show'); 
     navBgOverlayElt.classList.remove('active'); 
     document.body.style = 'visibility: visible; height: initial; width: 100%; overflow-x: hidden;'; 
 }
 
-navIconElt.addEventListener('click', navOpen); 
-navCloseElt.addEventListener('click', navClose); 
-navBgOverlayElt.addEventListener('click', navClose); 
+if(navIconElt){ // condition to only execute if there is an elt, no error but it does not work
+    navIconElt.addEventListener('click', navOpen); 
+}
+if(navCloseElt){
+    navCloseElt.addEventListener('click', navClose); 
+}
+if(navBgOverlayElt){
+    navBgOverlayElt.addEventListener('click', navClose); 
+}
+
+// aos
+AOS.init({
+    offset: 200,
+    delay: 100,
+    duration: 400,
+    easing: 'ease', 
+    once: false, 
+    mirror: false, 
+    anchorPlacement: 'top-bottom' 
+})
